c1b52b0882f08b2b499cba1913f935799a0a55af
Mesut Tasci <slackermesut@gmail.com>
2020-03-29 00:05:56 +0300

    repl updated

--- src/repl.rs
+++ src/repl.rs
@@ -2,9 +2,9 @@ use std::io;
 use crate::token::Token;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
-use crate::ast::{Program, Statement, Expression, Ident};
 
 pub fn start() {
+
     loop {
         let mut buf = String::new();
         match io::stdin().read_line(&mut buf) {
@@ -12,7 +12,15 @@ pub fn start() {
                 let mut lexer = Lexer::new(buf.as_str());
                 let mut p = Parser::new(lexer);
                 let prog = p.parse();
-                for x in prog {
+                if p.get_errors().len() > 0 {
+                    let errors = p.get_errors();
+                    errors.iter().map(|e| {
+                        println!("Parse error: {}", e);
+                    });
+                    continue
+                }
+
+                for x in prog.iter() {
                     println!("{}", x);
                 }
             }
